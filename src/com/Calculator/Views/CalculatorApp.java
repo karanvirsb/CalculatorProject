385ac73238a66a491d62c1ac108694d970ed92f9
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-01-09 13:40:28 -0500

    added all the buttons, created the layout and created a new css

--- src/com/Calculator/Views/CalculatorApp.java
+++ src/com/Calculator/Views/CalculatorApp.java
@@ -1,6 +1,7 @@
 package com.Calculator.Views;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,11 +17,16 @@ public class CalculatorApp extends Application {
         primaryStage.setTitle("Calculator");
         primaryStage.getIcons().add(new Image("com/Calculator/Assets/calculator.png"));
         GridPane root = new GridPane();
+        root.setAlignment(Pos.BASELINE_CENTER);
 
-        Label calFinal = new Label("");
+
+        Label calFinal = new Label("text");
         TextField calInput = new TextField();
         calInput.setPromptText("Enter a number");
 
+        calInput.setPrefWidth(375);
+
+        //these are the button for the app
         Button calZero = new Button("0");
         Button calOne = new Button("1");
         Button calTwo = new Button("2");
@@ -31,16 +37,63 @@ public class CalculatorApp extends Application {
         Button calSeven = new Button("7");
         Button calEight = new Button("8");
         Button calNine = new Button("9");
+
+        //these are the signs
         Button calEqual = new Button("=");
         Button calPlus = new Button("+");
+        Button calClearAll = new Button("CE");
+        Button calClear = new Button("C");
+        Button calBackSpace = new Button ("<-");
+        Button calDivision = new Button("/");
+        Button calMultiplication = new Button("*");
+        Button calMinus = new Button("-");
+        Button calNegate = new Button("+/-");
+        Button calDot = new Button (".");
 
-        
+        //adding the text and label in
+        root.add(calFinal, 0,0);
+        root.add(calInput, 0,1, 4, 1);
+
+        //Adding the buttons in for the top row
+        root.add(calClearAll, 0, 2);
+        root.add(calClear, 1,2);
+        root.add(calBackSpace, 2, 2);
+        root.add(calDivision, 3, 2);
+
+        //Adding the buttons for the second from top
+        root.add(calSeven, 0, 3);
+        root.add(calEight, 1, 3);
+        root.add(calNine, 2, 3);
+        root.add(calMultiplication, 3, 3);
+
+        //Adding the buttons for the thrid row
+        root.add(calFour, 0, 4 );
+        root.add(calFive, 1, 4 );
+        root.add(calSix, 2, 4 );
+        root.add(calMinus, 3, 4 );
+
+        //Addinh the buttons for the fourth row
+
+        root.add(calOne, 0, 5 );
+        root.add(calTwo, 1, 5 );
+        root.add(calThree, 2, 5 );
+        root.add(calPlus, 3, 5 );
+
+        //Adding buttons for the sixth row
+        root.add(calNegate, 0, 6 );
+        root.add(calZero, 1, 6 );
+        root.add(calDot, 2, 6 );
+        root.add(calEqual, 3, 6 );
+
+
 
         //setting up the scene and width
+
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setWidth(375);
         primaryStage.setHeight(500);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
