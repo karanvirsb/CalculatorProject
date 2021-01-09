f45df84d673476ae016cd1d857f3903c9d5b8446
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-01-09 12:26:48 -0500

    Added few buttons, the label and textfield for entering the calculations

--- src/com/Calculator/Views/CalculatorApp.java
+++ src/com/Calculator/Views/CalculatorApp.java
@@ -1,6 +1,10 @@
 package com.Calculator.Views;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -11,9 +15,30 @@ public class CalculatorApp extends Application {
        //setting up the app
         primaryStage.setTitle("Calculator");
         primaryStage.getIcons().add(new Image("com/Calculator/Assets/calculator.png"));
-
         GridPane root = new GridPane();
 
+        Label calFinal = new Label("");
+        TextField calInput = new TextField();
+        calInput.setPromptText("Enter a number");
+
+        Button calZero = new Button("0");
+        Button calOne = new Button("1");
+        Button calTwo = new Button("2");
+        Button calThree = new Button("3");
+        Button calFour = new Button("4");
+        Button calFive = new Button("5");
+        Button calSix = new Button("6");
+        Button calSeven = new Button("7");
+        Button calEight = new Button("8");
+        Button calNine = new Button("9");
+        Button calEqual = new Button("=");
+        Button calPlus = new Button("+");
+
+        
+
+        //setting up the scene and width
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.setWidth(375);
         primaryStage.setHeight(500);
         primaryStage.show();
